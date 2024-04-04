8e56a35a6648e217b4cc9786c2dae7c33ee3ce4e
Tair00 <suleimanov.t.i.20@gmail.com>
2024-04-04 17:59:52 +0300

    start working on payment

--- app/src/main/java/com/example/coffeetime/Activity/ContactDetailActivity.java
+++ app/src/main/java/com/example/coffeetime/Activity/ContactDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -20,11 +21,25 @@ public class ContactDetailActivity extends Activity {
     private String token;
     private Integer quantity;
     private ApiService apiService;
+    TextView priceTextView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_detail);
+        priceTextView = findViewById(R.id.price);
         quantity = getIntent().getIntExtra("quantity", 0);
+        int price;
+
+        if (quantity <= 15) {
+            price = 700;
+        } else if (quantity <= 30) {
+            price = 1350;
+        } else {
+            price = 1900;
+        }
+
+        String priceString = String.format("%d руб", price);
+        priceTextView.setText(priceString);
         System.out.println("   " + quantity);
         token = getIntent().getStringExtra("access_token");
     }
