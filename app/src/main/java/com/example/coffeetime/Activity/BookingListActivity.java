6830ba83c3bca6160ca10b42e861b299e57bb2e4
Tair00 <suleimanov.t.i.20@gmail.com>
2024-04-17 21:33:01 +0300

    Booking

--- app/src/main/java/com/example/coffeetime/Activity/BookingListActivity.java
+++ app/src/main/java/com/example/coffeetime/Activity/BookingListActivity.java
@@ -2,5 +2,125 @@ package com.example.coffeetime.Activity;
 
 import android.app.Activity;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coffeetime.Adapter.BookingListAdapter;
+
 public class BookingListActivity extends Activity {
+    private RecyclerView recyclerView;
+    private BookingListAdapter adapter;
+    private List<BookingItem> bookingList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_list);
+
+        recyclerView = findViewById(R.id.view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        bookingList = new ArrayList<>();
+        adapter = new BookingListAdapter(BookingListActivity.this, bookingList);
+        recyclerView.setAdapter(adapter);
+
+        // Выполните GET-запрос к серверу для получения данных
+        executeGetRequest();
+    }
+
+    private void executeGetRequest() {
+        String token = getIntent().getStringExtra("access_token"); // Получение значения токена
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://losermaru.pythonanywhere.com/reservation";
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Обработка успешного ответа от сервера
+                        parseResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Обработка ошибки запроса
+                        Toast.makeText(BookingListActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        System.out.println("-------------------" + error.getMessage());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token); // Добавление заголовка авторизации
+                return headers;
+            }
+        };
+
+        queue.add(request);
+    }
+    private void parseResponse(JSONArray response) {
+        try {
+            boolean hasNewApprovedItems = false;
+
+            for (int i = 0; i < response.length(); i++) {
+                JSONObject jsonObject = response.getJSONObject(i);
+                Integer id = jsonObject.getInt("restaurant_id");
+                String status = jsonObject.getString("status");
+
+                if ("approved".equals(status)) {
+                    String picture = jsonObject.getString("picture");
+                    String date = jsonObject.getString("day");
+                    String time = jsonObject.getString("time");
+                    String number = jsonObject.getString("number");
+                    String name = jsonObject.getString("name");
+
+                    BookingItem booking = new BookingItem(status, picture, date, time, name, id, number);
+
+                    if (!bookingList.contains(booking)) {
+                        bookingList.add(booking);
+                        hasNewApprovedItems = true;
+                    }
+
+                    fetchRestaurantName(id, booking);
+                }
+            }
+
+            adapter.notifyDataSetChanged();
+
+            if (hasNewApprovedItems) {
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("MyTag", "Ошибка при разборе ответа: " + e.getMessage());
+            Toast.makeText(BookingListActivity.this, "Error parsing response", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void fetchRestaurantName(int id, BookingItem booking) {
+        String url = "https://losermaru.pythonanywhere.com/restaurant/" + id;
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String name = response.getString("name");
+                            booking.setName(name);
+                            adapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        queue.add(request);
+    }
 }
